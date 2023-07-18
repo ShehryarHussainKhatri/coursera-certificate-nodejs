eaf7e8aceb1575b89ae4e1458a81398f6e960333
ShehryarHussainKhatri <133497713+ShehryarHussainKhatri@users.noreply.github.com>
2023-07-18 15:49:28 +0500

    Create booksdb.js

---
+++ router/booksdb.js
@@ -0,0 +1,14 @@
+let books = {
+    1: {"ISBN": "", "author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
+    2: {"ISBN": "", "author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
+    3: {"ISBN": "", "author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
+    4: {"ISBN": "", "author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
+    5: {"ISBN": "", "author": "Unknown","title": "The Book Of Job", "reviews": {} },
+    6: {"ISBN": "", "author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
+    7: {"ISBN": "", "author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
+    8: {"ISBN": "10001", "author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
+    9: {"ISBN": "", "author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
+    10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+}
+
+module.exports=books;
